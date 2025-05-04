1cf086faf1cf12f4debadf3d5dd0f069936420cc
IndraAlFauzan <nurulindra35@gmail.com>
2025-05-04 16:51:36 +0700

    setup main.go

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	r := gin.Default() // untuk membuat router baru
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Println("Server running on port " + port) // untuk menampilkan pesan bahwa server sudah berjalan
+	log.Fatal(r.Run(":" + port))                  // untuk menjalankan server pada port yang ditentukan
+
+}
